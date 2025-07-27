ef8decdc2b2651aff59f73e673cb8f4fa4a80d1d
arean82 <38061300+Arean82@users.noreply.github.com>
2025-07-27 21:35:48 +0530

    Update init.rb

--- init.rb
+++ init.rb
@@ -3,8 +3,8 @@ Redmine::Plugin.register :add_date_formats_with_day_names do
   author 'bokos'
   description 'Adds selectable date formats with day names to settings'
   version '0.0.2'
-  url 'https://github.com/bokos/add_date_formats_with_day_names'
-  author_url 'https://github.com/bokos/add_date_formats_with_day_names'
+  url 'https://github.com/Arean82/add_date_formats_with_day_names'
+  author_url 'https://github.com/Arean82/add_date_formats_with_day_names'
 
   RedmineApp::Application.config.after_initialize do
     SettingsHelper.send(:include, AddDateFormatsWithDayNames)
